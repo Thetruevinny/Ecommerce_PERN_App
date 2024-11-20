f7f67f448ff7578e62f73be11aea72846a1962de
Thetruevinny <abhik.vinod14@gmail.com>
2024-11-20 18:22:07 +0000

    Added userIcon in top right of screen to redirect users to user information page

--- view/src/Components/Navigation/Navigation.js
+++ view/src/Components/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import Styles from './Navigation.module.css';
 import {useDispatch} from 'react-redux';
 import { refreshCart } from '../Cart/CartSlice';
+import userIcon from '../../Photos/UserIcon.png';
 
 // Creating Navbar in Root component
 function Navigation() {
@@ -38,6 +39,9 @@ function Navigation() {
             navigate('/login');
         }
     }
+    const navigateUser = () => {
+        navigate('/user');
+    }
 
     return (
         <div className={Styles.nav}>
@@ -47,6 +51,11 @@ function Navigation() {
                 <NavLink to='/cart'><li>Cart</li></NavLink>
                 {verified ? <button onClick={onLogout}>Logout</button> : null}
             </ul>
+            {verified ? 
+                <div onClick={navigateUser} className={Styles.user}>
+                    <img src={userIcon} alt='User Image Icon' />
+                </div> 
+            : null}
         </div>
     );
 }
